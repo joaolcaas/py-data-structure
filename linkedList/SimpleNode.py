ff669118290370fd04013fae397c3db50021337e
joaolcaas <joao.felipe@ccc.ufcg.edu.br>
2019-01-15 22:54:50 -0300

    now each structure have it owns file

---
+++ linkedList/SimpleNode.py
@@ -0,0 +1,28 @@
+class SimpleNode:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+    
+    def get_value(self):
+        """
+        Return a node Value
+        """
+        return self.value
+
+    def get_next(self):
+        """
+        Return the next node or None
+        """
+        return self.next
+    
+    def set_next(self,node):
+        """
+        Set a the next node Value
+        """
+        self.next = node
+        return self
+    
+    def has_next(self):
+        if(self.get_next() != None):
+            return True
+        return False
---
+++ linkedList/TestLinked.py
@@ -0,0 +1,42 @@
+from structure import LinkedList as lk
+from structure import SimpleNode as no
+
+
+n1 = no.SimpleNode(130)
+n2 = no.SimpleNode(2)
+n3 = no.SimpleNode(11)
+a = lk.LinkedList()
+a.append_node(n1)
+#er = a.isEmpty()
+#print er
+#b = a.findMin()
+#print('menor')
+#print(b)
+#print('--------')
+#a.append_node(n2)
+#a.percorror_linked()
+#c = a.findMin()
+#print('menor')
+#print(c)
+#b = a.get_head()
+#c = b.get_next()
+a.append_node(n2)
+a.append_node(n3)
+print('list 1')
+a.percorror_linked()
+print('----- tostring-------')
+b = a.tostring()
+print b
+'''
+#print('list emtpy')
+#a.clear_all()
+a.percorror_linked()
+d = a.findMax()
+print d
+copy = a.copy()
+print('linked a')
+a.percorror_linked()
+print('linked copy')
+copy.percorror_linked()
+'''
+
