49a68b03588f684b1039de5656f7a321dc2aa3e2
joaolcaas <joao.felipe@ccc.ufcg.edu.br>
2019-01-21 12:14:04 -0300

    bst delet

--- bst/BST.py
+++ bst/BST.py
@@ -8,19 +8,6 @@ class BST():
     def __init__(self):
         self.root = None
     
-    def populate_root(self,value):
-        """
-        Populate the first bst's node(root)
-
-        Args:
-            node: a node that contains a values
-        """
-        if(self.root == None):
-            self.root = nodeBST(value)
-        else:
-            raise Exception('Root not empty')
-            return
-    
     def add_node(self,value):
         """
         Function to add nodes into the bst tree. 
@@ -37,39 +24,41 @@ class BST():
 
         while True:
             if(actual == None):
+                actual = nodeBST(value)
+                actual.prev = prev
+                
+                if(prev == None):
+                    self.root = nodeBST(value)
+
+                elif(value < prev.get_value()):
+                    prev.left = actual
+                else:
+                    prev.rigth = actual
+
                 break
 
             elif(actual.get_value() == value):
                 raise Exception('Equals node')
                 
-
             elif(actual.get_value() > value):
                 prev = actual
                 actual = actual.get_left()
                 
-                
-
             elif(actual.get_value() < value):
                 prev = actual
                 actual = actual.get_rigth()
-                
-
-        
-        #actual now is the node        
-        
-        actual = nodeBST(value)
-        
-        #his dad is his previous
-        
-        actual.prev = prev
-
-        #his father now have the reference for him as left or rigth
-        if(value < prev.get_value()):
-            prev.left = actual
-        else:
-            prev.rigth = actual
 
     
+    def is_empty(self):
+        """
+            Check if bst tree has elements
+        """
+
+        if(self.root == None):
+            return True
+        else:
+            return False
+
     def delete_node(self,value):
         """
         Rules:
@@ -79,4 +68,66 @@ class BST():
                 No left child: set parent of deleted node to left child of deleted node
             Node has two childrens -> find the largest in left subtree and turn it the new node
             
+            Args:
+            
         """
+
+        prev = None
+        actual = self.root
+
+        if(actual == None):
+            raise Exception("BST is empty")
+            return 
+
+        while True:
+            
+            if(actual == None):
+                #There is no value
+                raise Exception("Value not found")
+                return 
+
+            elif(actual.get_value() == value):
+                #achou o valor
+                break
+
+            elif(actual.get_value() > value):
+                #vai para a esquerda
+                prev = actual
+                actual = actual.get_left()
+
+            elif(actual.get_value() < value):
+                #vai para a direita
+                prev = actual
+                actual = actual.get_rigth()
+        
+        left_chil = actual.left
+        rigth_chil = actual.rigth
+        
+        #leaf node
+        if(left_chil == None and rigth_chil == None):
+            #root case
+            if(prev == None):
+                self.root = None
+            else:
+                if(value < prev.get_value()):
+                    prev.left = None
+                else:
+                    prev.rigth = None
+        
+        #left node is not empty
+        elif(left_chil != None and rigth_chil == None):
+            #root case
+            if(prev == None):
+                self.root = left_chil
+            else:
+                prev.prev.left = actual
+                actual.prev = prev.prev
+
+        #rigth node is not empty 
+        elif(left_chil == None and rigth_chil != None):
+            #root case
+            if(prev == None):
+                self.root = rigth_chil
+            else:
+                prev.prev.rigth = actual
+                actual.prev = prev.prev
--- bst/test.py
+++ bst/test.py
@@ -1,11 +1,14 @@
 from BST import BST as bst 
 
 bst_tree = bst()
+
+bst_tree.add_node(2)
 print(bst_tree.root)
-bst_tree.populate_root(1)
-print(bst_tree.root.get_value())
 bst_tree.add_node(-1)
-bst_tree.add_node(10)
-print(bst_tree.root.left.get_previous().get_value())
-print(bst_tree.root.rigth.get_previous().get_value())
-print(bst_tree.root.left.get_value())
+bst_tree.add_node(-2)
+bst_tree.add_node(-3)
+print(bst_tree.root.get_left().get_value())
+bst_tree.delete_node(-2)
+print(bst_tree.root.get_left().get_value())
+#print(bst_tree.root)
+print(bst_tree.is_empty())
